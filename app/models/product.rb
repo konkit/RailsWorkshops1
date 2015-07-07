4f389c9457a38a3806ccf32b34a9f1a9f76cf8a1
Łukasz Tenerowicz <konkit@gmail.com>
2015-07-07 17:27:26 +0200

    Task 4, 5, 6 done, all specs are passing

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,11 @@ class Product < ActiveRecord::Base
   belongs_to :category
   belongs_to :user
   has_many :reviews
+
+  validates_presence_of :title, :description, :price
+  validates :price, numericality: true, format: { with: /\A\d+(.\d{1,2})?\z/   }
+
+  def average_rating
+    reviews.inject(0) { |sum, review|  sum + review.rating } / reviews.count().to_f
+  end
 end
