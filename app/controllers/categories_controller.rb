971e985d4b7060fea0239d9f6dc90d19a6e0f695
Łukasz Tenerowicz <konkit@gmail.com>
2015-07-08 11:10:51 +0200

    Added presence validation to Category, fixed controllers causing test failures

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -19,7 +19,7 @@ class CategoriesController < ApplicationController
   end
 
   def create
-    category = Category.new(category_params)
+    self.category = Category.new(category_params)
     if category.save
       redirect_to category, notice: 'Category was successfully created.'
     else
